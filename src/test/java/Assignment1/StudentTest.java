b98e5f9926e36607911b790f37bba3bf45179c73
Marc Blain <marcblain1@gmail.com>
2018-10-24 12:24:32 +0100

    Test Class Added

--- src/test/java/Assignment1/StudentTest.java
+++ src/test/java/Assignment1/StudentTest.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import assignment1.Student;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class StudentTest {
+    @Test
+    public void generateUsername() throws Exception {
+        final Student a = new Student("MarcoBlano", "26-04-1998", 15349941);
+        final String expected = "MarcoBlano20";
+        final String actual = a.generateUsername();
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
-}
+}     
