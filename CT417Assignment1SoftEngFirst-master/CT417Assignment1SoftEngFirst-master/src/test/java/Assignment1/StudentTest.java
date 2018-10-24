f4b26ae07db2826f1fbb03340e72a6bfbe5b1bfd
Marc Blain <marcblain1@gmail.com>
2018-10-24 16:04:38 +0100

    Improvements

---
+++ CT417Assignment1SoftEngFirst-master/CT417Assignment1SoftEngFirst-master/src/test/java/Assignment1/StudentTest.java
@@ -0,0 +1,22 @@
+package Assignment1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import assignment1.Student;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StudentTest {
+    @Test
+    public void generateUsername() throws Exception {
+        final Student a = new Student("MarcoBlano", "26-04-1998", 15349941);
+        final String expected = "MarcoBlano20";
+        final String actual = a.generateUsername();
+        assertEquals(expected, actual);
+    } 
+
+}     
